0d27ac5ef638d0f0b776846a65d07ccca5121f37
Aaron Long <aaronlong0712@gmail.com>
2021-12-27 12:39:34 -0500

    Complete!

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,43 @@ document.body.onload = onLoad()
 
 function createCard(array) {
     array.forEach(obj => {
-        var newDiv = document.createElement("div")
-        newDiv.setAttribute("class", "w3-card")
-        newDiv.setAttribute("style", "width: 63%; margin: auto;")
-        document.body.appendChild(newDiv)
 
-        var nameP = document.createElement("p")
-        nameP.innerText = obj.name
-        nameP.setAttribute("style", "padding-left: 40px; font-size: 18px; padding-top: 10px; padding-bottom: 10px;")
-        newDiv.appendChild(nameP)
+        var spacerDiv = document.createElement("div")
+        spacerDiv.setAttribute("style", "height:20px")
+        document.body.appendChild(spacerDiv)
 
+        var containerDiv = document.createElement("div")
+        containerDiv.setAttribute("class", "w3-card")
+        containerDiv.setAttribute("style", "width: 63%; margin: auto; word-wrap: break-word;")
+        document.body.appendChild(containerDiv)
+
+        var dataDiv = document.createElement("div")
+        dataDiv.setAttribute("style", "display: inline-block; padding-left: 40px; padding-top: 10px; width: 70%")
+        containerDiv.appendChild(dataDiv)
+
+        var nameText = document.createElement("p")
+        nameText.innerText = obj.name
+        nameText.setAttribute("style", "margin: 0px; padding-left: 30px; font-size: 18px; font-weight: 900; width: 70%")
+        dataDiv.appendChild(nameText)
+
+        var descriptionText = document.createElement("p")
+        descriptionText.innerText = obj.description
+        descriptionText.setAttribute("style", "margin: 0px; padding-left: 30px; font-size: 14px; padding-bottom: 10px; width: 70%; display: inline-block;")
+        dataDiv.appendChild(descriptionText)
+
+        var statusContainer = document.createElement("div")
+        const stylepreset = "display: inline-block; padding-left: 75px; padding-right: 75px; margin: auto; text-align: center; margin-top: 40px; margin-bottom: 40px; border-radius: 25px; "
+        if (obj.status == 0) {
+            statusContainer.innerHTML = "<span style='color: White; font-weight: 900;'>Not Started</span>"
+            statusContainer.setAttribute("style", `${stylepreset} background-color:rgb(224, 45, 45);`)
+        } else if (obj.status == 1) {
+            statusContainer.innerHTML = "<span style='color: White; font-weight: 900;'>In Progress</span>"
+            statusContainer.setAttribute("style", `${stylepreset} background-color:rgb(235, 231, 40);`)
+        } else if (obj.status == 2) {
+            statusContainer.innerHTML = "<span style='color: White; font-weight: 900;'>Live</span>"
+            statusContainer.setAttribute("style", `${stylepreset} background-color:rgb(42, 235, 68);`)
+        }
+        containerDiv.appendChild(statusContainer)
     })
 }
 
